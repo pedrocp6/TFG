8456669d63feb0fa0c906b6338878c55a815e1be
pedrocp6 <pedro.cabello.02@gmail.com>
2026-06-04 18:14:03 +0200

    feat: MPC validation in simulink

--- mis_pruebas/hil_can_control/matlab/Simulation/launch_simulation.m
+++ mis_pruebas/hil_can_control/matlab/Simulation/launch_simulation.m
@@ -13,16 +13,16 @@ Tend = time_ref(end);
 % v_ref = vel;           %trackdrive
 % v_ref = 1+20*time_ref;  %acceleration
 % v_ref = 10 +0*time_ref;   %skidpad
-% v_ref = 1.5*vx_target-8;  % mi AutoX
-v_ref = linspace(1,16,length(time_ref));    % mi skidpad
+v_ref = 1.5*vx_target-8;  % mi AutoX
+% v_ref = linspace(1,16,length(time_ref));    % mi skidpad
 
 % v_ref = vel;
 v_target = timeseries(v_ref,time_ref);
 
 % Reference trajectory
-% k_ref = k;             %trackdrive
+k_ref = k;             %trackdrive
 % k_ref = k*0;            %acceleration
-k_ref = k*0+1/9.125;   %skidpad
+% k_ref = k*0+1/9.125;   %skidpad
 k_target = timeseries(k_ref,time_ref);
 
 % Initial values
